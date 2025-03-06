37fe58369e7977b2a905d265a610f744a078c87f
CryptoGodSon <106864633+CryptGodSon@users.noreply.github.com>
2025-03-06 03:37:05 +0100

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,14 @@
+const hre = require("hardhat");
 
+async function main() {
+  const CryptoFund = await hre.ethers.getContractFactory("CryptoFund");
+  const cryptoFund = await CryptoFund.deploy();
+  await cryptoFund.deployed();
+
+  console.log(`CryptoFund deployed to: ${cryptoFund.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
